3a8d5787b9c1219f8c97c7592608ab5265005b07
Deivid Francis <deividfrancisdeoliveira@gmail.com>
2022-11-03 23:01:44 -0400

    feat: finaliza parte 2 da aula 3

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,15 @@
-import { NativeBaseProvider, Center, Text, StatusBar } from "native-base";
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from "native-base";
 
-import { THEME } from "./src/styles/theme";
-import { SignIn } from "./src/pages/SignIn";
 import { Loading } from "./src/components/Loading";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { Pools } from "./src/pages/Pools";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +26,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
